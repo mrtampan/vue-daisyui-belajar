39a56b5451e9e97ab1aac13e583bd94482a43cae
mrtampan <mrtampan54@gmail.com>
2023-11-13 16:10:42 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from './Navbar.vue';
+import Table from './Table.vue';
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+<Navbar />
+<Table />
+</template>
+
